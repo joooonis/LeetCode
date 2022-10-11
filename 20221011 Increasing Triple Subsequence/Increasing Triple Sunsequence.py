7f3999785147c0bf14cb166cdadd79e4a3e11f97
jooooonis <xowns3213@naver.com>
2022-10-11 22:44:53 +0900

    20221011 : Increasing Triple Sunsequence

---
+++ 20221011 Increasing Triple Subsequence/Increasing Triple Sunsequence.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def increasingTriplet(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+                
+        first, second = float('inf'), float('inf')
+        
+        for third in nums:
+            
+            if second < third: return True
+            if third <= first: first= third    
+            else:  
+                second = third 
