22be3d813a8a6e87555e60f3d4be3deb60fb252b
jooooonis <xowns3213@naver.com>
2023-04-08 00:46:37 +0900

    BFS, DFS

---
+++ Tags/Breath-First Search/1020. Number of Enclaves/1020. Number of Enclaves.js
@@ -0,0 +1,41 @@
+const numEnclaves = (grid) => {
+  let answer = 0;
+  const row = grid.length;
+  const col = grid[0].length;
+  const visited = Array(row)
+    .fill()
+    .map(() => Array(col).fill(false));
+  const isBoundary = ([x, y]) =>
+    x === 0 || x === row - 1 || y === 0 || y === col - 1;
+  const bfs = (start) => {
+    const dx = [1, -1, 0, 0];
+    const dy = [0, 0, 1, -1];
+    queue = [start];
+    let isArea = isBoundary(start);
+    let count = 1;
+    while (queue.length) {
+      const [x, y] = queue.shift();
+      visited[x][y] = true;
+      for (let i = 0; i < 4; i++) {
+        const nx = x + dx[i];
+        const ny = y + dy[i];
+        if (nx >= 0 && nx < row && ny >= 0 && ny < col) {
+          if (!visited[nx][ny] && grid[nx][ny] === 1) {
+            queue.push([nx, ny]);
+            visited[nx][ny] = true;
+            isArea = isArea ? isArea : isBoundary([nx, ny]);
+            count++;
+          }
+        }
+      }
+    }
+    if (!isArea) answer += count;
+  };
+
+  for (let i = 0; i < row; i++) {
+    for (let j = 0; j < col; j++) {
+      if (grid[i][j] === 1 && !visited[i][j]) bfs([i, j]);
+    }
+  }
+  return answer;
+};
